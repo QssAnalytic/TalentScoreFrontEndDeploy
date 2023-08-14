67eb40a6d6e3b9fe5173d547bf014941bec93d94
Nicat Rzayev <99348762+RzayevNicat@users.noreply.github.com>
2023-08-14 11:16:14 +0400

    router

--- src/main.tsx
+++ src/main.tsx
@@ -4,30 +4,31 @@ import './index.css';
 import {createBrowserRouter, RouterProvider} from 'react-router-dom';
 import {Provider} from 'react-redux';
 import {store} from './state/store';
-import {Children} from 'react';
 import Landing from 'pages/Landing/Landing';
 import Login from 'pages/Auth/Login/Login';
 import Register from 'pages/Auth/Register/Register';
 import Stages from 'pages/Stages';
+import ErrorPage from 'components/ErrorPage';
 const router = createBrowserRouter([
 	{
 		path: '/',
 		element: <App />,
+		errorElement: <ErrorPage />,
 		children: [
 			{
 				path: '/',
 				element: <Landing />
 			},
 			{
-				path: '/login',
+				path: 'login',
 				element: <Login />
 			},
 			{
-				path: '/register',
+				path: 'register',
 				element: <Register />
 			},
 			{
-				path: '/stages/:stageSlug/:subStageSlug',
+				path: 'stages/:stageSlug/:subStageSlug',
 				element: <Stages />
 			}
 		]
