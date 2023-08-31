cfab88a9262493f6cc774c894c8f5ae2605ac089
alihasanli <ahasanli639.dev@gmail.com>
2023-08-31 16:29:48 +0400

    adding certificate

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './assets/reset.css'
+import Certificate from "./Certificate/Certificate"
+
+function App() {
+  return (
+    <>
+      <Certificate />
+    </>
+  )
+}
+
+export default App
