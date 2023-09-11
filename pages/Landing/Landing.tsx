1183b272291117bd1d5b6a77629da49b6a35efa9
magaNasib <mehemmednessibov3@gmail.com>
2023-09-11 11:16:00 +0400

    last

--- pages/Landing/Landing.tsx
+++ pages/Landing/Landing.tsx
@@ -1,4 +1,4 @@
-import Chatbot from 'components/Chatbot/Chatbot';
+import Chatbot from './components/Chatbot/Chatbot';
 import About from './components/About';
 import Footer from './components/Footer';
 import HeroSection from './components/HeroSection';
