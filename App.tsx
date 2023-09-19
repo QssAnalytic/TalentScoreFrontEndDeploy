71b83c8f6d70eaae3c2711b41a00cb31e03dfa7e
magaNasib <mehemmednessibov3@gmail.com>
2023-09-19 12:26:56 +0400

    last

--- App.tsx
+++ App.tsx
@@ -14,7 +14,7 @@ const App = () => {
 	return (
 		<div className="min-h-screen bg-qss-background font-inter relative">
 			<Outlet />
-			{showReport && (
+			{!showReport && (
 				<div className="fixed bg-black/60 z-40 inset-0" onClick={() => dispatch(setShowReport(!showReport))}>
 					<div className="flex gap-4 w-full h-full pl-32 scale-75">
 						<img src={ReportFront} alt="reportFront" className=" aspect-[3/4]" />
